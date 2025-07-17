04e425a001b4bbad46cd031b8184feedfa5d6c19
JP Pincheira <jpincheira@gmail.com>
2025-07-17 18:23:54 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,18 +6,18 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Talked.io</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <Header title="Welcome to my app!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Coming soon! This is a placeholder page for the Talked.io project.
         </p>
       </main>
 
-      <Footer />
+      {/* <Footer /> */}
     </div>
   )
 }
